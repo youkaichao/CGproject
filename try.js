8b0a3529d1f073f2908d91d5cdd3e0662ece0d62
youkaichao <youkaichao@126.com>
2021-04-27 11:38:51 +0800

    maybe done

--- try.js
+++ try.js
@@ -49,7 +49,7 @@ for(let i = 0; i < N; ++i)
     ctx.fillRect(points[i].x - 2.5, points[i].y - 2.5, 5, 5);
 }
 
-// 算法代码正式开始,返回1代表左边
+// 算法代码正式开始,返回1代表左边，是在屏幕坐标系下，但是以人的感觉的左边
 function toLeft(start, end, point)
 {
     let a = {'x': end.x - start.x, 'y': end.y - start.y};
@@ -72,6 +72,11 @@ function toLeft(start, end, point)
 
 function point_equal(a, b) {
     return a.x === b.x && a.y === b.y;
+}
+
+
+function negateP(p) {
+    return {'x': - p.x, 'y': -p.y, 'i': p.i};
 }
 
 
@@ -160,72 +165,92 @@ class Trapezoid {
     }
 }
 
-// 按照y坐标升序排列，屏幕方向从上到下
-let indices = [...Array(points.length).keys()];
-indices.sort((a, b) => points[a].y - points[b].y);
+function onePass(points) {
+    // 按照y坐标升序排列，屏幕方向从上到下
+    let indices = [...Array(points.length).keys()];
+    indices.sort((a, b) => points[a].y - points[b].y);
 
-let sweepline = []; // 扫描线，用于存放四边形
-let output = []; // 存放切出来的多边形
-for(let i = 0; i < indices.length; ++i)
-{
-    // in_ts 表示包含p的四边形 
-    let index = indices[i];
-    let p = points[index];
-    let in_ts = [];
-    for(let j = 0; j < sweepline.length; ++j)
+    let sweepline = []; // 扫描线，用于存放四边形
+    let output = []; // 存放切出来的多边形
+    for(let i = 0; i < indices.length; ++i)
     {
-        let t = sweepline[j];
-        if(t.in_test(p))
+        // in_ts 表示包含p的四边形 
+        let index = indices[i];
+        let p = points[index];
+        let in_ts = [];
+        for(let j = 0; j < sweepline.length; ++j)
         {
-            in_ts.push([t, j]);
+            let t = sweepline[j];
+            if(t.in_test(p))
+            {
+                in_ts.push([t, j]);
+            }
         }
-    }
 
-    // 取出p两边的点
-    let prev = (index + points.length - 1) % points.length;
-    prev = points[prev];
-    let next = (index + 1) % points.length;
-    next = points[next];
+        // 取出p两边的点
+        let prev = (index + points.length - 1) % points.length;
+        prev = points[prev];
+        let next = (index + 1) % points.length;
+        next = points[next];
 
-    // p 代表一个新的四边形，则两边的点还没check，比它低
-    if(in_ts.length === 0)
-    {
-        sweepline.push(new Trapezoid(p, prev, next));
-    }
-    else if(in_ts.length === 1)
-    {
-        let [t, t_index] = in_ts[0];
-        if(point_equal(p, t.head) && point_equal(p, t.tail))
-        {// 四边形封闭了
-            sweepline.splice(t_index, 1);
-            output.push(t);
+        // p 代表一个新的四边形，则两边的点还没check，比它低
+        if(in_ts.length === 0)
+        {
+            sweepline.push(new Trapezoid(p, prev, next));
         }
-        else if(point_equal(p, t.head))
-        {// 是左边的虚顶点
-            t.helper = p;
-            t.chain.splice(0, 0, (prev.y > next.y ? prev : next));
+        else if(in_ts.length === 1)
+        {
+            let [t, t_index] = in_ts[0];
+            if(point_equal(p, t.head) && point_equal(p, t.tail))
+            {// 四边形封闭了
+                sweepline.splice(t_index, 1);
+                t.chain.splice(0, 1);
+                output.push(t);
+            }
+            else if(point_equal(p, t.head))
+            {// 是左边的虚顶点
+                t.helper = p;
+                t.chain.splice(0, 0, (prev.y > next.y ? prev : next));
+            }
+            else if(point_equal(p, t.tail))
+            {// 是右边的虚顶点
+                t.helper = p;
+                t.chain.push(prev.y > next.y ? prev : next);
+            }else{
+                // 在四边形内部，该split了
+                let [t1, t2] = t.split(p, next, prev);
+                sweepline.splice(t_index, 1, t1, t2);
+            }
         }
-        else if(point_equal(p, t.tail))
-        {// 是右边的虚顶点
-            t.helper = p;
-            t.chain.push(prev.y > next.y ? prev : next);
-        }else{
-            // 在四边形内部，该split了
-            let [t1, t2] = t.split(p, next, prev);
-            sweepline.splice(t_index, 1, t1, t2);
+        else if(in_ts.length === 2)
+        {// 两个T要merge
+            let [t1, t1_index] = in_ts[0];
+            let [t2, t2_index] = in_ts[1];
+            sweepline.splice(t1_index, 1);
+            sweepline.splice(t2_index - 1, 1);
+            let s = new Trapezoid();
+            s.merge(t1, t2);
+            sweepline.push(s);
         }
     }
-    else if(in_ts.length === 2)
-    {// 两个T要merge
-        let [t1, t1_index] = in_ts[0];
-        let [t2, t2_index] = in_ts[1];
-        sweepline.splice(t1_index, 1);
-        sweepline.splice(t2_index - 1, 1);
-        let s = new Trapezoid();
-        s.merge(t1, t2);
-        sweepline.push(s);
-    }
+    return output;
 }
 
-output.forEach(t => t.draw(ctx));
-sweepline.forEach(t => t.draw(ctx));
+function MonotoneDecomp(points) {
+    let output = onePass(points);
+    let answer = [];
+    output.forEach(t1 => {
+        onePass(t1.chain.map(negateP)).forEach(t2 => {
+            answer.push(t2.chain.map(negateP));
+        });
+    });
+    return answer;
+}
+
+let answer = MonotoneDecomp(points);
+
+answer.forEach(each => {
+    let t = new Trapezoid();
+    t.chain = each;
+    t.draw(ctx);
+});
